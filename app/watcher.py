2413a2feccd4c86bd72a7ea4278fa311d6a627ae
Nikita Glushkov <glushkov.nik1@gmail.com>
2024-05-22 22:37:59 -0300

    Change main app to class

--- app/watcher.py
+++ app/watcher.py
@@ -1,72 +1,71 @@
 import inquirer
 from database_manager import DatabaseManager
 
-db_manager = DatabaseManager()
+class WatcherApp:
+    def __init__(self):
+        self.db_manager = DatabaseManager()
+        self.is_running = True
 
-def add_watched_movie():
-    print("Вы выбрали: Добавить просмотренный фильм")
+    def add_watched_movie(self):
+        pass
 
-def last_watched_movies():
-    print("Вы выбрали: Последние просмотренные фильмы")
+    def last_watched_movies(self):
+        pass
 
-def find_a_movie():
-    questions = [inquirer.Text("title", message="Enter the title")]
-    title = inquirer.prompt(questions).get("title")
-    result = db_manager.find_movies(title)
-    print_movies(result)
+    def find_a_movie(self):
+        questions = [inquirer.Text("title", message="Enter the title")]
+        title = inquirer.prompt(questions).get("title")
+        result = self.db_manager.find_movies(title)
+        self.print_movies(result)
 
-def print_movies(movies: list) -> None:
-    if not movies:
-        confirm_question = [
-            inquirer.Confirm(
-                "find_another",
-                message="Movie not found. Find another movies?",
-                default=True,
-            )
-        ]
-        answer = inquirer.prompt(confirm_question)
-        if answer["find_another"]:
-            find_a_movie()
-        return
+    def print_movies(self, movies: list) -> None:
+        if not movies:
+            confirm_question = [
+                inquirer.Confirm(
+                    "find_another",
+                    message="Movie not found. Find another movies?",
+                    default=True,
+                )
+            ]
+            answer = inquirer.prompt(confirm_question)
+            if answer["find_another"]:
+                self.find_a_movie()
+            return
     
-    choices = [(f"{movie['title']} ({movie['year']})", movie["external_id"]) for movie in movies]
-    movies_questions = [
-        inquirer.List(
-            "choosed_movie",
-            message="Choose movie",
-            choices=choices
-        )
-    ]
-    answers = inquirer.prompt(movies_questions)
-    print(answers)
-    
-def exit_program():
-    global is_run
-    is_run = False
-    print("Exiting the program...")
-
-def main_menu():
-    global is_run
-    is_run = True
-
-    action_functions = {
-        "Add watched movie": add_watched_movie,
-        "Last watched movies": last_watched_movies,
-        "Find a movie": find_a_movie,
-        "Exit": exit_program
-    }
-    
-    while is_run:
-        questions = [
+        movies_questions = [
             inquirer.List(
-                "action",
-                message="Choose an action",
-                choices=list(action_functions.keys())
+                "choosed_movie",
+                message="Choose movie",
+                choices=[(f"{movie['title']} ({movie['year']})", movie["external_id"]) for movie in movies]
             )
         ]
-        answers = inquirer.prompt(questions)
-        action = answers["action"]
-        action_functions[action]()
+        answers = inquirer.prompt(movies_questions)
+        print(answers)
+    
+    def exit_program(self):
+        self.is_running = False
+        print("Exiting the program...")
+
+    def main_menu(self):
+        action_functions = {
+            "Add watched movie": self.add_watched_movie,
+            "Last watched movies": self.last_watched_movies,
+            "Find a movie": self.find_a_movie,
+            "Exit": self.exit_program
+        }
+
+        while self.is_running:
+            questions = [
+                inquirer.List(
+                    "action",
+                    message="Choose an action",
+                    choices=list(action_functions.keys())
+                )
+            ]
+            answers = inquirer.prompt(questions)
+            action = answers["action"]
+            action_functions[action]()
 
 if __name__ == "__main__":
-    main_menu()
+    app = WatcherApp()
+    app.main_menu()
