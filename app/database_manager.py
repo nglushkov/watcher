d3c52f1f0c41bb5faf6b80b4a9dc767e775bead6
Nikita Glushkov <glushkov.nik1@gmail.com>
2024-05-26 00:38:58 -0300

    Change sort in find_movies action

--- app/database_manager.py
+++ app/database_manager.py
@@ -15,7 +15,7 @@ class DatabaseManager:
     def find_movies(self, title) -> list:
         with self.connect() as conn:
             cursor = conn.cursor(dictionary=True)
-            query = "SELECT id, external_id, title, YEAR(release_date) year FROM movies WHERE LOWER(title) LIKE LOWER(%s) ORDER BY release_date;"
+            query = "SELECT id, external_id, title, YEAR(release_date) year FROM movies WHERE LOWER(title) LIKE LOWER(%s) ORDER BY release_date DESC"
             like_value = f"%{title}%"
             cursor.execute(query, (like_value,))
             movies = cursor.fetchall()
